f2abbea0c6c6abe3eb77284d8ad3cf102e47afda
helioz11 <kevin.lesli3@yahoo.com>
2018-04-08 01:41:13 -0700

    Triangle Spec pass

--- triangle.c
+++ triangle.c
@@ -5,5 +5,5 @@ int triangle_type(int a, int b, int c)
 {
 	if(a<triangle_min_edge || a>triangle_max_edge || b<triangle_min_edge || b>triangle_max_edge
 || c<triangle_min_edge || c>triangle_max_edge) return 0;
-	else return -1;
+	else return -1; 
 }
