ca39245a2c0391d747e75a087ac6b8da17d9e6e7
helioz11 <kevin.lesli3@yahoo.com>
2018-04-09 07:11:32 -0700

    commission test PASS

--- commission.c
+++ commission.c
@@ -4,7 +4,10 @@
 
 double commission(int lock, int stock, int barrel)
 {
-	int commission = 0;
+	double commission = 0;
+
+	if(lock<min_lock || lock>max_lock || stock<min_stock || stock>max_stock || barrel<min_barrel || barrel>max_barrel) return INVALID_SALE;
+
 	int total_sales = (lock*lock_price) + (stock*stock_price) + (barrel*barrel_price);
 	if(total_sales>1800.0)
 	{
@@ -15,7 +18,7 @@ double commission(int lock, int stock, int barrel)
 	else if(total_sales>1000.0)
 	{
 		commission = 0.10 * 1000.0;
-		commission = commission * 0.15 * (total_sales - 1000.0);
+		commission = commission + 0.15 * (total_sales - 1000.0);
 	}
 	else commission = 0.10 * total_sales;
 	
